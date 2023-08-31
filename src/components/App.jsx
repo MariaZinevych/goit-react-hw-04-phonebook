d2d856c814439fa7bfc673da16b3801171fa2535
MariaZinevych <123274407+MariaZinevych@users.noreply.github.com>
2023-09-01 00:43:51 +0200

    created

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,36 +1,34 @@
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import { Phonebook } from './Name/name';
 
 import { GlobalStyle } from 'Global.styled';
 import { ContactList } from './Contactlist/contactList';
 import { Filter } from './Filter/filter';
 
-export class App extends Component {
-  state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
-    filter: '',
-  };
+const contact = [
+  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+];
 
-  componentDidMount() {
-    const savedFilter = localStorage.getItem('phoneContact');
-    if (savedFilter !== null) {
-      this.setState({ contacts: JSON.parse(savedFilter) });
-    }
-  }
+export const App = () => {
+  const [contacts, setContacts] = useState(
+    JSON.parse(window.localStorage.getItem('phoneContact')) ?? contact
+  );
+  const [filter, setFilter] = useState('');
 
-  componentDidUpdate(prevProps, prevState) {
-    if (prevState.contacts !== this.state.contacts) {
-      localStorage.setItem('phoneContact', JSON.stringify(this.state.contacts));
-    }
-  }
+  const normalizedFilter = filter.toLocaleLowerCase();
+  const visiebleList = contacts.filter(contact =>
+    contact.name.toLocaleLowerCase().includes(normalizedFilter)
+  );
 
-  addQuiz = newQuiz => {
-    const isInContacts = this.state.contacts.some(
+  useEffect(() => {
+    window.localStorage.setItem('phoneContact', JSON.stringify(contacts));
+  }, [contacts]);
+
+  const addQuiz = newQuiz => {
+    const isInContacts = contacts.some(
       ({ name }) => name.toLowerCase() === newQuiz.name.toLowerCase()
     );
 
@@ -39,41 +37,26 @@ export class App extends Component {
 
       return;
     }
-    this.setState(prevState => {
-      return {
-        contacts: [...prevState.contacts, newQuiz],
-      };
-    });
+    setContacts(prevState => [...prevState, newQuiz]);
   };
 
-  onChangeFilter = event => {
-    this.setState({ filter: event.target.value });
+  const onChangeFilter = event => {
+    setFilter({ filter: event.target.value });
   };
 
-  removeContact = contactId => {
-    this.setState(prevState => {
-      return {
-        contacts: prevState.contacts.filter(({ id }) => id !== contactId),
-      };
-    });
+  const removeContact = contactId => {
+    setContacts(prevState => prevState.filter(({ id }) => id !== contactId));
   };
 
-  render() {
-    const normalizedFilter = this.state.filter.toLocaleLowerCase();
-    const visiebleList = this.state.contacts.filter(contact =>
-      contact.name.toLocaleLowerCase().includes(normalizedFilter)
-    );
+  return (
+    <>
+      <h1>Phonebook</h1>
+      <Phonebook onAdd={addQuiz} />
+      <h2>Contacts</h2>
 
-    return (
-      <>
-        <h1>Phonebook</h1>
-        <Phonebook onAdd={this.addQuiz} />
-        <h2>Contacts</h2>
-
-        <Filter value={this.state.filter} onChange={this.onChangeFilter} />
-        <ContactList onValues={visiebleList} onDelete={this.removeContact} />
-        <GlobalStyle />
-      </>
-    );
-  }
-}
+      <Filter value={filter} onChange={onChangeFilter} />
+      <ContactList onValues={visiebleList} onDelete={removeContact} />
+      <GlobalStyle />
+    </>
+  );
+};
